b61730e3f89c39b5b07550eca05e5e6c5a4e0148
Haovnlon <95330981+Haovnlon@users.noreply.github.com>
2025-07-11 06:30:51 +0700

    build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -7,32 +7,9 @@ dependencies {
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
+    language = "vi"
+    description = "Xem phim từ sieutamphim.org (Helvid stream)"
+    authors = listOf("Haovnlon")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
 }
