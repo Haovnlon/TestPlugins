3ca81502b168e57644c1bf3e6e189f2092766ad3
Haovnlon <95330981+Haovnlon@users.noreply.github.com>
2025-07-11 07:04:21 +0700

    settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ val disabled = listOf<String>()
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(":ExampleProvider")
+        include(dir.name)
     }
 }
 
